0d51d271f9e59ad90b0cd6a36ef70b76c829ad08
Antonin Januska <antonin@antjanus.com>
2013-04-25 11:39:25 -0400

    First commit

---
+++ lesson1/index.php
@@ -0,0 +1,36 @@
+<?php
+
+class SelfOrderingArray{
+    protected $arr = array();
+    protected $type = '';
+
+    public function __construct($type = 'NUMERIC', $defaultArr = array()){
+        $this->type = $type;
+        $this->arr = $defaultArr;
+        
+    }
+
+    public function addElement($el){
+       array_push($this->arr, $el);
+       if($this->type == 'NUMERIC'){
+        sort($this->arr, SORT_NUMERIC);
+    }
+    elseif($this->type == 'STRING'){
+        sort($this->arr, SORT_STRING);
+    }
+    else{
+        sort($this->arr, SORT_REGULAR);
+    }
+    return $this->arr;
+}
+    public function getArray(){
+        return $this->arr;
+    }
+
+}
+
+$newArr = new SelfOrderingArray('STRING', array('orange', 'apple', 'watermelon'));
+$newArr->addElement('pear');
+print_r($newArr->getArray());
+
+?>
